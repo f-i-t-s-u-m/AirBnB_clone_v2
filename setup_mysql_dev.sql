bf498d0de08c91d71ada98a30a1b17db8ef12049
fitsum belay <fibabirr@gmail.com>
2022-03-10 04:20:00 -0500

    fix new user and database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+''' add new user and database '''
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
