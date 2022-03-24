bd77f9e9e79245daacaa60694209bb7dc3bd3130
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-24 12:57:48 +0300

    fix shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,11 @@ echo 'Hello World!' > data/web_static/shared/index.html
 ln -sf data/web_static/releases/test data/web_static/current
 sudo bash -c 'sudo -hR  chown ubuntu:ubuntu data'
 str='\n\tlocation /hbnb_static {\n\t\talias /home/data/web_static/current;\n\t}\n'
-sudo sed -i "48s|.*|$str|" /etc/nginx/sites-available/default
+sudo sed -i "47s|.*|$str|" /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default etc/nginx/sites-enabled/
-systemctl is-active nginx -q && sudo systemctl reload nginx || sudo systemctl start nginx
+if systemctl is-active nginx -q
+then
+  sudo systemctl reload nginx
+else
+  sudo systemctl start nginx
+fi
