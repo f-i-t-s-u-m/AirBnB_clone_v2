1be00ef742cf7c322df59938b6c259a2fc181fae
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-24 13:43:06 +0300

    fix fx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # init project
 sudo apt-get -y install nginx
-bash -c 'mkdir -p data/web_static/releases/test'
-bash -c 'mkdir -p data/web_static/shared'
-echo 'Hello World!' > data/web_static/releases/test/index.html
-ln -sf data/web_static/releases/test data/web_static/current
-sudo bash -c 'sudo -hR  chown ubuntu:ubuntu data'
-str='\n\tlocation /hbnb_static {\n\t\talias /home/data/web_static/current;\n\t}\n'
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo 'Hello World!' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+sudo bash -c 'sudo chown -hR ubuntu:ubuntu /data'
+str='\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'
 sudo sed -i "47s|.*|$str|" /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
 if systemctl is-active nginx -q
