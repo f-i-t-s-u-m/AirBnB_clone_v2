9ac1906f0e0644ed211b997faa7544ff00828308
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-24 13:46:00 +0300

    fix fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,15 +4,9 @@ sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
 echo 'Hello World!' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo bash -c 'sudo chown -hR ubuntu:ubuntu /data'
 str='\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'
 sudo sed -i "47s|.*|$str|" /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
-if systemctl is-active nginx -q
-then
-  sudo systemctl reload nginx
-else
-  sudo systemctl start nginx
-fi
-
+sudo /etc/init.d/nginx restart
