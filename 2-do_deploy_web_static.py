9f9a1709b2ecdb2de94c666759cc45d68b79d928
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-25 00:27:17 +0300

    fix 2-do

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,16 +4,17 @@
 from fabric.api import run, env
 import os
 
-
 env.hosts = ['44.192.23.250', '3.238.29.156', '3.236.241.248']
 
-def test(archive_path):
+
+def do_deploy(archive_path):
+    """ deploy file """
     if not os.path.exists(archive_path):
         return False
     put('{}, /tmp/'.format(archive_path))
-    run('tar zxf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_path, archive_path.split('.')[0]))
+    run('tar zxf /tmp/{} -C /data/web_static/releases/{}'\
+        .format(archive_path, archive_path.split('.')[0]))
     run('rm -r /tmp/{}'.format(archive_path))
-    run('ln -sf /data/web_static/releases/{} /data/web_static/current/'/
-            .format(archive_path.splite('.')[0]))
+    run('ln -sf /data/web_static/releases/{} /data/web_static/current/'\
+        .format(archive_path.split('.')[0]))
     return True
