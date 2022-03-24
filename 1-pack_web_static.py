fe3474c653203d47631ca6ad0cb90f3c01e874d4
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-24 23:00:36 +0300

    add 1-p

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" python file for fabric """
+
+
+from fabric.api import local
+import os
+import datetime
+
+def do_pack():
+    """ pack web files """
+
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    filename = 'versions/web_static_' + datetime.datetime.now().strftime("%G%m%d%H%M%S") + '.tgz'
+    print('Packing web_static to {}'.format(filename))
+    local('tar -zcvf {} web_static'.format(filename))
