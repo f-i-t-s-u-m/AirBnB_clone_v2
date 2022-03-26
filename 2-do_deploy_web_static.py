61f32e62305e4fde575adb00fadfade801b940cf
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-26 19:55:38 +0300

    fix 2-do*

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,16 +12,17 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     basename = os.path.basename(archive_path)
+    path = '/data/web_static/releases'
     put(archive_path, '/tmp/')
-    run('mkdir -p /data/web_static/releases/{}'.format(basename.split('.')[0]))
-    run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'\
+    run('mkdir -p {}/{}'.format(path, basename.split('.')[0]))
+    run('tar -zxf /tmp/{} -C /data/web_static/releases/{}'
         .format(basename, basename.split('.')[0]))
     run('rm /tmp/{}'.format(basename))
-    run('mv /data/web_static/releases/{0}/web_static/* /data/web_static/releases/{0}'\
-            .format(basename.split('.')[0]))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(basename.split('.')[0]))
+    run('mv {0}/{1}/web_static/* /data/web_static/releases/{1}'
+        .format(path, basename.split('.')[0]))
+    run('rm -rf {}/{}/web_static'.format(path, basename.split('.')[0]))
     run('rm -rf /data/web_static/current')
-    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'\
-        .format(basename.split('.')[0]))
+    run('ln -sf {}/{}/ /data/web_static/current'
+        .format(path, basename.split('.')[0]))
     print('New version deployed!')
     return True
