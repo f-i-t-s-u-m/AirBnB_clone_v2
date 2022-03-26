ec2f4e2c5cdaacca0c47fe106eadeb7d29c96b0e
f-i-t-s-u-m <fibabirr@gmail.com>
2022-03-26 11:34:33 +0300

    try 0-s

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,15 +2,11 @@
 # init project
 
 sudo apt-get -y install nginx
-mkdir -p /data
-mkdir /data/web_static
-mkdir /data/web_static/releases
-mkdir /data/web_static/shared
-mkdir /data/web_static/releases/test
-touch /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 echo 'Hello World!' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo bash -c 'sudo chown -hR ubuntu:ubuntu data'
+sudo bash -c 'sudo chown -hR ubuntu:ubuntu /data'
 str="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n"
 sudo sed -i "47s|.*|$str|" /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
