16f1c60c845562faf916bb610febcad5a365161f
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-10 01:42:54 +0300

    add 7 state and templates

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" list state """
+
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_all():
+    return render_template('7-states_list.html', states=storage.all())
+
+
+@app.teardown_appcontext
+def teardown_appcontext(response_or_exc):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
