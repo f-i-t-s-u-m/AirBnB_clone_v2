a2bbe5bac7ae3c25b791ec1be35e36ab994dc26f
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-10 14:39:27 +0300

    fix 7-state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,13 +2,14 @@
 """ list state """
 
 from flask import Flask, render_template
-from models import storagei, state
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def state_all():
-    return render_template('7-states_list.html', states=storage.all(state.State))
+    return render_template('7-states_list.html', states=storage.all(State))
 
 
 @app.teardown_appcontext
