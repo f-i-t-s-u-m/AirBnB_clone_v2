03ea925b510f2be12adc7bf36da3f8af046cf3eb
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-10 23:47:54 +0300

    fix 2-c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     """ index function """
-    return "Hello HBNB!"
+    return "Hello HBNB! "
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ hbnb route """
-    return "HBNB"
+    return "HBNB "
 
 
 @app.route('/c/<text>', strict_slashes=False)
