6d4e6431fe647ca19d0a8dc3d45ad0b71ad808f8
f-i-t-s-u-m <fibabirr@gmail.com>
2022-04-10 18:51:49 +0300

    fix doc 9 3

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" list states python file """
+""" list states python file
+    contains states and teardown function
+"""
 
 from flask import Flask, render_template
 from models import storage
@@ -11,7 +13,9 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def states(id=None):
-    """ states function """
+    """ states function
+        check if id is none
+    """
     if id is None:
         return render_template('9-states.html', states=storage.all(State))
     else:
@@ -24,7 +28,9 @@ def states(id=None):
 
 @app.teardown_appcontext
 def teardown_appcontext(response_or_exc):
-    """ teardown after done """
+    """ teardown after done
+        close database connection
+    """
     storage.close()
 
 
